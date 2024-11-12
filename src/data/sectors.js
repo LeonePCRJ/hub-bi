cb53614f4c23325869bd299caee6488909d8cb52
Leone <leone.smtr@gmail.com>
2024-11-11 22:17:29 -0300

    Reestruturação das pastas

---
+++ src/data/sectors.js
@@ -0,0 +1,80 @@
+export const sectorsData = [
+    {
+      id: 'ads',
+      nome: "ADS - Administração Setorial",
+      bis: [
+        {
+          id: 'ads-rh',
+          nome: "Recursos Humanos",
+          imagem: "/images/grh.png",
+          url: "https://app.powerbi.com/view?r=eyJrIjoiM2VmZWRlYmEtZmJmYi00MzAwLWEyZTEtMzgzYWIwZWVlZWNiIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
+        },
+        {
+          id: 'ads-contratos',
+          nome: "Contratos",
+          imagem: "/images/contratos.png",
+          url: "https://app.powerbi.com/view?r=eyJrIjoiZmRjNWQ4NDEtZjFjYi00Njg3LTkzZTQtMmFjMTE4NzY4NzUyIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
+        }
+      ]
+    },
+    {
+      id: 'ccco',
+      nome: "CCCO - Coordenadoria de Contratos de Concessão",
+      bis: [
+        {
+          id: 'ccco-stpl',
+          nome: "STPL",
+          imagem: "/images/stpl.png",
+          url: "https://lookerstudio.google.com/reporting/c3920c69-cfd8-4b4e-a6f4-aee99ea0ff02/page/firqD"
+        },
+        {
+          id: 'ccco-indices',
+          nome: "Histórico de Índices",
+          imagem: "/images/indices.png",
+          url: "https://app.powerbi.com/view?r=eyJrIjoiZmU0NzAyNzctMWEyNS00YzFiLTllMWQtOTUwMGRlMGFhMDhmIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
+        },
+        {
+          id: 'ccco-multa-contratual',
+          nome: "Multa Contratual",
+          imagem: "/images/multa_contratual.png",
+          url: "https://app.powerbi.com/view?r=eyJrIjoiM2RjYmY2YTktODUzMC00MmQzLWFmN2UtYTE2MWVlYmFmNzczIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
+        },
+        {
+          id: 'ccco-multa-disciplinar',
+          nome: "Multa Disciplinar",
+          imagem: "/images/multa_disciplinar.png",
+          url: "https://app.powerbi.com/view?r=eyJrIjoiYTkyMTc5MmEtMzI0Ni00ZDJlLTk0ODktZTM4NjQ2Yjc2MzFmIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
+        },
+        {
+          id: 'ccco-rmc-bip',
+          nome: "Histórico RMC e BIP",
+          imagem: "/images/rmc_bip.png",
+          url: "https://app.powerbi.com/view?r=eyJrIjoiOGNkZjBiNjMtNjRhZC00ZDNiLTkyYjQtN2NlMGNmZTFjODAwIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
+        }
+      ]
+    },
+    {
+      id: 'adi',
+      nome: "ADI - Assessoria de Demandas Institucionais",
+      bis: [
+        {
+          id: 'adi-acompanhamento',
+          nome: "Acompanhamento e Controle",
+          imagem: "/images/ata.png",
+          url: "https://app.powerbi.com/view?r=eyJrIjoiZTcwYjU0ZmEtMTBiMi00Y2EzLTkzZDctZjYyOTUzMjhlYTE2IiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9&pageName=ReportSection0c555e4164e3a67400da"
+        }
+      ]
+    },
+    {
+      id: 'corin',
+      nome: "CORIN - Comissão de Recursos de Infração",
+      bis: [
+        {
+          id: 'corin-processos',
+          nome: "Controle Processos",
+          imagem: "/images/corin.png",
+          url: "https://app.powerbi.com/view?r=eyJrIjoiNGYxODE0NTMtMGU3YS00NWExLWFhZTktZjBiOGU4YzYzOTNiIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
+        }
+      ]
+    }
+  ];
